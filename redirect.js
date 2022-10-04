c89cb1b3f8d09dd4629f972e4df498f96feb681d
Eduardo <eduardo@milocredit.com>
2022-10-04 14:26:46 -0400

    add milo.io permanent redirects

--- redirect.js
+++ redirect.js
@@ -15,50 +15,46 @@ function buildDomainUrl(uri) {
   return newurl;
 }
 
+function redirectTo(newurl) {
+  var response = {
+    statusCode: 301,
+    statusDescription: 'Found',
+    headers:
+        { "location": { "value": buildDomainUrl(newurl) } }
+    }
+  return response;
+}
+
 function handler(event) {
   var request = event.request;
   var headers = request.headers;
   var host = headers.host.value;
   var uri = request.uri;  
   var querystring = request.querystring;
-  
-  //console.log(request);
+
+  if (host === 'www.milocredit.com' || host === 'dev.milocredit.com') {
+    var newurl = `https://${host}${uri}`
+    return redirectTo(newurl);
+  }
   
   if (host.startsWith('blog')) {
       var newUri = buildBlogUrl(uri, querystring)
       var newurl = `https://${host.replace('blog','www')}${newUri}`
-      var response = {
-          statusCode: 301,
-          statusDescription: 'Found',
-          headers:
-              { "location": { "value": newurl } }
-          }
-      return response;
+      return redirectTo(newurl);
   }
   
   if (host.startsWith('test')) {
       var newUri = buildBlogUrl(uri, querystring)
       var newurl = `https://${host.replace('test','dev')}${newUri}`
-      var response = {
-          statusCode: 301,
-          statusDescription: 'Found',
-          headers:
-              { "location": { "value": newurl } }
-          }
-      return response;
+      return redirectTo(newurl);
   }
 
   if (!host.startsWith('www') && !host.startsWith('dev')) {
       var newurl = `https://www.${host}${uri}`
-      var response = {
-          statusCode: 301,
-          statusDescription: 'Found',
-          headers:
-              { "location": { "value": newurl } }
-          }
-      return response;
+      return redirectTo(newurl);
   }
+
   return request;
 }
 
-module.exports = handler;
+(module || {}).exports = handler;
--- redirect.test.js
+++ redirect.test.js
@@ -18,7 +18,7 @@ test('blog should redirect to /blog/', () => {
   const response = {
     "headers": {
       "location": {
-        "value": "https://www.milocredit.com/blog/hello-milo-digital-banking-solution/"
+        "value": "https://www.milo.io/blog/hello-milo-digital-banking-solution/"
       }
     }, 
     "statusCode": 301, 
@@ -50,7 +50,7 @@ test('blog lang=es should redirect to /es/blog/', () => {
   const response = {
     "headers": {
       "location": {
-        "value": "https://www.milocredit.com/es/blog/hello-milo-digital-banking-solution/"
+        "value": "https://www.milo.io/es/blog/hello-milo-digital-banking-solution/"
       }
     }, 
     "statusCode": 301, 
@@ -77,7 +77,89 @@ test('non WWW should -> WWWW', () => {
   const response = {
     "headers": {
       "location": {
-        "value": "https://www.milocredit.com"
+        "value": "https://www.milo.io"
+      }
+    }, 
+    "statusCode": 301, 
+    "statusDescription": "Found"
+  }
+
+  expect(handler(event)).toEqual(response);
+});
+
+
+test('milocredit.com -> www.milo.io', () => {
+
+  const event = {
+    "request": {
+      "method": "GET",
+      "uri": "",
+      "headers": {
+        "host": {
+          "value": "milocredit.com"
+        },
+      },
+    }
+  }
+
+  const response = {
+    "headers": {
+      "location": {
+        "value": "https://www.milo.io"
+      }
+    }, 
+    "statusCode": 301, 
+    "statusDescription": "Found"
+  }
+
+  expect(handler(event)).toEqual(response);
+});
+
+test('www.milocredit.com -> www.milo.io', () => {
+
+  const event = {
+    "request": {
+      "method": "GET",
+      "uri": "",
+      "headers": {
+        "host": {
+          "value": "www.milocredit.com"
+        },
+      },
+    }
+  }
+
+  const response = {
+    "headers": {
+      "location": {
+        "value": "https://www.milo.io"
+      }
+    }, 
+    "statusCode": 301, 
+    "statusDescription": "Found"
+  }
+
+  expect(handler(event)).toEqual(response);
+});
+
+test('dev.milocredit.com -> dev.milo.io', () => {
+
+  const event = {
+    "request": {
+      "method": "GET",
+      "uri": "",
+      "headers": {
+        "host": {
+          "value": "dev.milocredit.com"
+        },
+      },
+    }
+  }
+
+  const response = {
+    "headers": {
+      "location": {
+        "value": "https://dev.milo.io"
       }
     }, 
     "statusCode": 301, 
